218297ab94a7e62f63395a2204f9de4817c109d9
quentinmathieu <quentin.mth@gmail.com>
2023-04-05 13:36:47 +0200

    close teams

--- _clear_script_.bat
+++ _clear_script_.bat
@@ -1,15 +1,21 @@
 @echo off
 
+ 
+
 echo Install programm
 set startup="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 if NOT exist %startup%\%~n0%~x0 (
     copy %0 %startup%\%~n0%~x0
     msg * "Installation complete !"
-) 
+)
+
+ 
 
 
 echo Clear broswers cache
 taskkill /F /IM msedge.exe
+
+ 
 
 RD /S /Q "%LocalAppData%\MicrosoftEdge\Cookies"
 RD /S /Q "%LocalAppData%\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\INetCookies"
@@ -22,6 +28,8 @@ RD /S /Q "%LocalAppData%\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\#!002
 RD /S /Q "%LocalAppData%\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\#!002\MicrosoftEdge\User\Default\DOMStore"
 RD /S /Q "%LocalAppData%\Local\Microsoft\Edge\User Data\Default\cache"
 
+ 
+
 del "%USERPROFILE%\AppData\Local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC" /s /f /q /a
 del "%LocalAppData%\Microsoft\Edge\User Data\Default\History" /S /Q
 del "%LocalAppData%\Microsoft\Edge\User Data\Default\Cache" /S /Q
@@ -33,21 +41,31 @@ del "%LocalAppData%\Microsoft\Edge\User Data\Default\Web Data" /S /Q
 del "%LocalAppData%\Microsoft\Edge\User Data\Default\Media History" /S /Q
 del "%LocalAppData%\Microsoft\Edge\User Data\Default\Cookies-Journal" /S /Q
 
+ 
+
 
 :: Parse the Local AppData sub path
 call :Expand xAppData "%%LocalAppData:%UserProfile%=%%"
+
+ 
 
 set "xFirefox=\mozilla\firefox\profiles"
 set "xChrome=\google\chrome\user data"
 set "xOpera1=\Local\Opera\Opera"
 set "xOpera2=\Roaming\Opera\Opera"
 
+ 
+
 :: Start at the User directory
 pushd "%UserProfile%\.."
+
+ 
 
 taskkill /F /IM firefox.exe
 taskkill /F /IM chrome.exe
 taskkill /F /IM opera.exe
+
+ 
 
 :: Loop through the Users
     for /D %%D in (*) do if exist "%%~fD%xAppData%" (
@@ -56,10 +74,14 @@ taskkill /F /IM opera.exe
         rd /s /q "%%~fD%xAppData%%xFirefox%"
     )
 
+ 
+
    rem Check for Chrome
    if exist "%%~fD%xAppData%%xChrome%" (
         rd /s /q "%%~fD%xAppData%%xChrome%"
     )
+
+ 
 
     rem Check for Opera
     if exist "%%~fD%xAppData%%xOpera1%" (
@@ -72,48 +94,73 @@ taskkill /F /IM opera.exe
 popd
 goto End
 
+ 
+
 
 ::::::::::::::::::::::::::::::
 :Expand <Variable> <Value>
 if not "%~1"=="" set "%~1=%~2"
 goto :eof
 
+ 
+
 
 :End
+
+ 
 
 echo:
 echo ----------------------------------------------------
 echo:
+
+ 
 
 echo Delete All Credentials
 cmdkey /delete:git:https://github.com
 cmdkey /delete:vscodevscode.github-authentication/github.auth
+
+ 
+
+taskkill /F /IM Teams.exe
+RMDIR %appdata%\Microsoft\Teams
 cmdkey /delete:teamsIv/teams
 cmdkey /delete:teamsKey/teams
+
+ 
 
 echo:
 echo ----------------------------------------------------
 echo:
+
+ 
 
 
 echo Delete .gitconfig in %USERPROFILE%
 del %userprofile%\.gitconfig
 
+ 
+
 echo:
 echo ----------------------------------------------------
 echo:
 
-echo Delete All files in laragon/www & dataBases & downloads
-Xcopy C:\laragon\www %USERPROFILE%\Documents\trash /E /H /C /I /Y
+ 
+
+echo Delete All files in laragon/www
+Xcopy C:\laragon\www %USERPROFILE%\trash /E /H /C /I /Y
 del /f /s /q "C:\laragon\www" 1>nul
-Xcopy C:\laragon\data\mysql %USERPROFILE%\Documents\data_trash /E /H /C /I /Y
+Xcopy C:\laragon\data\mysql %USERPROFILE%\data_trash /E /H /C /I /Y
 del /f /s /q "C:\laragon\data\mysql" 1>nul
 del /f /s /q "%USERPROFILE%\Downloads" 1>nul
+
+ 
 
 
 echo:
 echo -----------------------------------------------------
 echo:
+
+ 
 
 echo Chrome as default browser
 start chrome --make-default-browser
